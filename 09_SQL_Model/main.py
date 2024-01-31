97e8e3b7ac284c93d59618a6a1ebd2e0524afe17
Muhammad Rafi Ali <therafiali@gmail.com>
2024-01-31 07:13:05 +0500

    main.py

--- 09_SQL_Model/main.py
+++ 09_SQL_Model/main.py
@@ -1,1 +1,15 @@
-engine = create_engine('postgresql://postgres:rootadmin@localhost:5432/SQL_Model', echo=True)
+from typing import Optional
+from sqlmodel import SQLModel, Field, create_engine
+
+class Hero(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    secret_name: str
+    age: Optional[int] = None
+    
+    
+database_connection_str = "postgresql://postgres:rootadmin@localhost:5432/SQL_Model"    
+#echo true only development env not for production
+engine = create_engine(database_connection_str, echo=True)    
+
+SQLModel.metadata.create_all(engine)
