f84a6341b4a267fbd3c4f5e1ef2afcdc7284da50
Muhammad Rafi Ali <therafiali@gmail.com>
2024-01-31 07:36:12 +0500

    Create DB & Pydantic Models Using SQL Model

--- 09_SQL_Model/main.py
+++ 09_SQL_Model/main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from sqlmodel import SQLModel, Field, create_engine
+from sqlmodel import SQLModel, Field, create_engine,Session
 
 class Hero(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -12,4 +12,22 @@ database_connection_str = "postgresql://postgres:rootadmin@localhost:5432/SQL_Mo
 #echo true only development env not for production
 engine = create_engine(database_connection_str, echo=True)    
 
-SQLModel.metadata.create_all(engine)
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def create_hero():
+    hero1 = Hero(name="Rafi Ali",secret_name="Rafay",age=22)
+    hero2 = Hero(name="Abu Bakr",secret_name="Sadiq",age=42)
+    hero3 = Hero(name="Umer",secret_name="Farooq",age=32)
+    
+    session = Session(engine)
+    session.add(hero1)
+    session.add(hero2)
+    session.add(hero3)
+    session.commit()
+    session.close()
+    
+    
+if __name__ == "__main__":
+    create_db_and_tables()
+    create_hero()
